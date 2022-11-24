8d5d23b29f7125180477fad39d3af4e2d7698487
gnzjgo <gonzalojgo@gmail.com>
2022-11-24 23:19:20 +0100

    minor changes

--- demo-ecomm-svelte/src/routes/about/+page.svelte
+++ demo-ecomm-svelte/src/routes/about/+page.svelte
@@ -11,6 +11,7 @@ import GridTile from "../../lib/GridTile.svelte";
 
 <h1>Ecommerce Demo App used for Tinybird demos</h1>
 
+<h2>UX Personalization</h2>
 <p>
   Some of our customers use Tinybird to personalize the user experence in real time, being <a class="underline" href="https://medium.com/hotel-tech-stories/the-hotels-network-goes-real-time-adb748b6b124" target="_blank">THN</a>
 	 and <a class="underline" href="https://www.tinybird.co/case-studies/daily-dev-relies-on-tinybird-to-keep-devs-up-to-date-on-the-best-developer-news" target="_blank">Daily.dev</a> two great examples of this.
@@ -19,7 +20,7 @@ import GridTile from "../../lib/GridTile.svelte";
   Here we have an app that does realtime UX personalization in its most basic format, without taking the visitor id into account, just based on product popularity.<br>
 	Basically, we ingest an event with the product id and timestamp every time a product is viewed, clicked, or sold. We build a pipe in Tinybird that exposes an API endpoint on top of that data to reorder the products from the most viewed/sold/added-to-cart to the least one based, choosing the datasource to look up with the ranking parameter.</p>
 <div class="flex justify-center max-w-screen-md">
-	<img alt="tinybird data flow schema: user activity sends data to three datasources –sales, carts, views– and the three datasources feed an endpoint called ranking" class="h-72" src="drawings/basic-ranking.png">
+	<img alt="tinybird data flow schema: user activity sends data to a datasource called web_events. The datasource feeds an endpoint called ranking" class="h-72" src="drawings/basic-ranking.png">
 </div>
 <p>Check the data-project <a href="https://github.com/tinybirdco/ecommerce-svelte/tree/main/data-project" target="_blank">data-project</a> for more details.</p>
 
@@ -35,7 +36,9 @@ import GridTile from "../../lib/GridTile.svelte";
 <p>
 	Another common Tinybird use case is Operational Intelligence. The same data being used to rank products in the store is valuable information for the business. Capturing this data and presenting it to the organization in a <a class="underline" href="https://pocs.tinybird.co/commerce-ufa?token={token}" target="_blank">real-time dashboard</a> enables data-driven decisions.
 </p>
-
+<div class="flex justify-center max-w-screen-md">
+	<img alt="tinybird data flow improved schema: same diagram as before but with materialized views that show aggregations and several endpoints from there" class="h-72" src="drawings/improved-flowchart.png">
+</div>
 <h2>Web Analytics Tracker</h2>
 <p>
 	Following the <a class="underline" href="https://github.com/tinybirdco/web-analytics-starter-kit#readme" target="_blank">Starter Kit README</a> you can have a web tracker with Tinybird.
