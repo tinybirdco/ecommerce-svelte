35427ebbd95c08d8db7145439665a1f79d3b0e49
gnzjgo <gonzalojgo@gmail.com>
2022-12-12 12:07:59 +0100

    first-tremor-commit

---
+++ tremor-dashboard/components/KpiCard.tsx
@@ -0,0 +1,28 @@
+import {
+  BadgeDelta,
+  Block,
+  Card,
+  Flex,
+  Metric,
+  ProgressBar,
+  Text,
+} from '@tremor/react';
+
+type KpiCardProps = {
+  title: string;
+  metric: string;
+  prefix: string;
+} 
+// Single KPI card in the demo dashboard with sample inputs
+export default function KpiCard({title = 'defaultTitle', metric = 'defaultMetric', prefix =''}: KpiCardProps) {
+  return (
+      <Card maxWidth="max-w-sm">
+          <Flex alignItems="items-start">
+              <Block>
+                  <Text>{title}</Text>
+                  <Metric truncate={true}>{prefix} {metric}</Metric>
+              </Block>
+          </Flex>
+      </Card>
+  );
+}
