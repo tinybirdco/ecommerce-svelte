df2301d537fab491f3f7728025a2bc8e061fa70f
Rafa Moreno <rmoreno@tinybird.co>
2023-10-31 08:14:31 +0100

    fix typo

--- demo-ecomm-svelte/src/components/ProductTile.svelte
+++ demo-ecomm-svelte/src/components/ProductTile.svelte
@@ -48,7 +48,7 @@
         <div
           class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"
         >
-          {`Only ${product.status} left!`}
+          {`Only ${product.stock} left!`}
         </div>
       {/if}
       {#if product.stock_status === StockStatus.outOfStock}
