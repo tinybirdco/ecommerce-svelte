4f0b93d5640ef426080d61ebf03dad4bc022dbcc
gnzjgo <gonzalojgo@gmail.com>
2023-12-07 12:19:06 +0100

    Adding link to tremor dashboard

--- demo-ecomm-svelte/src/components/Footer.svelte
+++ demo-ecomm-svelte/src/components/Footer.svelte
@@ -13,6 +13,17 @@
         to rebalance stock.
       </div>
       <div class="mt-2 lg:text-2xl">
+        Visit the
+        <a
+          href="https://ecommerce-svelte-tremor-dashboard.vercel.app/"
+          class="mt-4 font-bold no-underline text-white hover:underline lg:text-2xl"
+          target="_blank"
+        >
+          Revenue dashboard
+        </a>
+        to check sales in real time.
+      </div>
+      <div class="mt-2 lg:text-2xl">
         View the
         <a
           href="https://github.com/tinybirdco/ecommerce-svelte"
